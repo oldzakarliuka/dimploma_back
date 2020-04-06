fbbdca6b0f948d3fa8526ed9b1a93c1bdb1209aa
Artem Zak <artyomzakarluka@gmail.com>
2020-04-06 19:57:02 +0300

    try with nbc

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, escape, request, json, jsonify
 from flask_restful import Resource, Api, reqparse
 from flask_cors import CORS, cross_origin
 
-#from nbc import NBC, prepare_text
+from nbc import NBC, prepare_text
 
 app = Flask(__name__)
 api = Api(app)
@@ -11,7 +11,7 @@ cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 # NaiveBayesianClassifier
-#nbc = NBC()
+nbc = NBC()
 
 
 @app.route('/')
@@ -23,14 +23,13 @@ def hello():
 def nbc_analyze():
     phrase = request.args.get("phrase", "hi there I\'m an API")
 
-    # rows = list(
-    #     map(lambda line: line.strip(),
-    #         prepare_text(phrase)
-    #         .split('.')
-    #         )
-    # )
-    #prediction = nbc.prediction(rows)
-    prediction = True
+    rows = list(
+        map(lambda line: line.strip(),
+            prepare_text(phrase)
+            .split('.')
+            )
+    )
+    prediction = nbc.prediction(rows)
     return jsonify(
         phrase=phrase,
         result=prediction
